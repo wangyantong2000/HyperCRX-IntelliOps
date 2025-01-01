b19ca7777a5ee75648cdd671548c2bc4cbc7809a
wangyantong2000 <18745996292@163.com>
2025-01-01 20:40:04 +0800

    feat: add pages

---
+++ src/pages/ContentScripts/index.ts
@@ -0,0 +1,19 @@
+import './index.scss';
+
+import './features/repo-activity-openrank-trends';
+import './features/developer-activity-openrank-trends';
+import './features/repo-header-labels';
+import './features/repo-fork-tooltip';
+import './features/repo-star-tooltip';
+import './features/repo-pr-tooltip';
+import './features/repo-issue-tooltip';
+import './features/perceptor-tab';
+import './features/perceptor-layout';
+import './features/repo-networks';
+import './features/developer-networks';
+import './features/oss-gpt';
+import './features/repo-activity-racing-bar';
+import './features/developer-hovercard-info';
+import './features/repo-sidebar-labels';
+import './features/fast-pr';
+import './features/auto-code-annotator';
